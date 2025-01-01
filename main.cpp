c65c92590897375f198695ab0584d18c948423b2
xen2cute <103986726+xen2cute@users.noreply.github.com>
2025-01-01 17:09:29 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ int main()
 	for (int i = 0; i < 17; i++) // 'i' is equal to the number of addresses scanned 
 	{
 		std::vector<std::string> client_dll_offsetList = { "dwCSGOInput", "dwEntityList", "dwGameEntitySystem", "dwGameEntitySystem_highestEntityIndex", "dwGameRules", "dwGlobalVars", "dwGlowManager", "dwLocalPlayerController", "dwLocalPlayerPawn", "dwPlantedC4", "dwPrediction", "dwSensitivity", "dwSensitivity_sensitivity", "dwViewAngles", "dwViewMatrix", "dwViewRender", "dwWeaponC4"};
-		std::cout << "HEX --> " << client_dll_offsetList[i] << " --> " << getAddress(client_dll_offsetList[i], 1) << std::endl;
+		std::cout << client_dll_offsetList[i] << " --> " << getAddress(client_dll_offsetList[i], 1) << std::endl;
 	}
 
 	Sleep(-1); // pause console indefinitly 
