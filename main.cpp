452ffe0b1c7b72503282b8da50dfed07fb2011a2
t0ughknuckles <103986726+t0ughknuckles@users.noreply.github.com>
2025-01-07 01:18:12 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,18 +1,20 @@
 #include <iostream>
 #include <Windows.h>
+#include <string>
 
-#include "autoupdater.hpp"
+#include "AutoUpdater.hpp"
 
 int main()
 {
-	std::vector<std::string> client_dll_offsetList = { "dwCSGOInput", "dwEntityList", "dwGameEntitySystem", "dwGameEntitySystem_highestEntityIndex", "dwGameRules", "dwGlobalVars", "dwGlowManager", "dwLocalPlayerController", "dwLocalPlayerPawn", "dwPlantedC4", "dwPrediction", "dwSensitivity", "dwSensitivity_sensitivity", "dwViewAngles", "dwViewMatrix", "dwViewRender", "dwWeaponC4" };
+	std::vector<std::string> addr1Array = { "dwCSGOInput", "dwEntityList" }; // offsets.hpp: each address only returns 1 result making no need to specify further than the address name.
+	std::vector<std::string> addr2Array = { "std::ptrdiff_t m_flDuckOffset", "std::ptrdiff_t m_nDuckTimeMsecs" }; // client_dll.hpp: to ensure finding the correct data we be more specific with the addr name
+	for (int i = 0; i < addr1Array.size(); i++)
+		std::cout << "offsets.hpp: " << addr1Array[i] << " | " << getAddress(addr1Array[i], 1) << std::endl;
 
-	for (int i = 0; i < client_dll_offsetList.size(); i++)
-		std::cout << client_dll_offsetList[i] << " --> " << getAddress(client_dll_offsetList[i], 1) << std::endl;
+	for (int i = 0; i < addr2Array.size(); i++)
+		std::cout << "client_dll.hpp: " << addr2Array[i] << " | " << getAddress(addr2Array[i], 2) << std::endl; 
 
-	session.CloseURL();
-	session.CloseSession();
-
+	closeWeb(session); // close the internet handle when we're done with it, rather than opening and closing it everytime the getAddr function is used.
 	Sleep(-1);
 
 	return 0;
