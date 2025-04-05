dd010f2e757f4a5827d4aec0465ad5532541e12c
t0ughknuckles <103986726+t0ughknuckles@users.noreply.github.com>
2025-04-04 22:53:59 -0400

    Update AutoUpdater.hpp

--- AutoUpdater.hpp
+++ AutoUpdater.hpp
@@ -9,44 +9,43 @@
 #include <sstream>
 #include <vector>
 #include <algorithm>
-#include "web.h"
+#include "Web.h"
 
 std::string uRL;
-URLSession session;
 
-inline void closeWeb(URLSession s)
+inline void closeWeb(URLSession session)
 {
-    s.CloseURL();
-    s.CloseSession();
+    session.CloseURL();
+    session.CloseSession();
 }
 
-inline std::vector<std::string> splitLines(const std::string& data) 
+inline std::vector<std::string> splitLines(const std::string& data)
 {
     std::vector<std::string> lines;
     std::istringstream stream(data);
     std::string line;
-    while (std::getline(stream, line)) 
+    while (std::getline(stream, line))
     {
         lines.push_back(line);
     }
-    
+
     return lines;
 }
 
-inline std::vector<std::string> extractLines(const std::vector<std::string>& lines, const std::string& word) 
+inline std::vector<std::string> extractLines(const std::vector<std::string>& lines, const std::string& word)
 {
     std::vector<std::string> results;
-    for (const auto& line : lines) 
+    for (const auto& line : lines)
     {
         size_t pos = line.find(word);
-        if (pos != std::string::npos) 
+        if (pos != std::string::npos)
             results.push_back(line.substr(pos + word.length()));
-        
+
     }
     return results;
 }
 
-std::ptrdiff_t getAddress(std::string addrName, int url)
+std::ptrdiff_t getAddress(URLSession session, std::string addrName, int url)
 {
     uRL = "https://raw.githubusercontent.com/a2x/cs2-dumper/refs/heads/main/output";
 
@@ -54,8 +53,8 @@ std::ptrdiff_t getAddress(std::string addrName, int url)
         uRL = uRL + "/offsets.hpp";
     else if (url == 2)
         uRL = uRL + "/client_dll.hpp";
-
-    if (!session.OpenSession()) 
+   
+    if (!session.OpenSession())
         std::cout << "Failed to open session." << std::endl;
 
     if (!session.OpenURL(uRL))
@@ -66,13 +65,12 @@ std::ptrdiff_t getAddress(std::string addrName, int url)
 
     for (const auto& result : results) {
         std::string str1;
-        str1 = ReplaceAll(result, "= ", "");
+        str1 = ReplaceAll(result, "=", "");
         str1 = ReplaceAll(str1, ";", "");
-        
+
         unsigned int decValue = std::stoul(str1, nullptr, 16);
         return decValue;
     }
 
     return 0;
 }
-
