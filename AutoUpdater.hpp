9cef85fcc28b6b1557e402f84c489b9548199a22
t0ughknuckles <103986726+t0ughknuckles@users.noreply.github.com>
2025-05-06 07:56:33 -0400

    Update AutoUpdater.hpp

--- AutoUpdater.hpp
+++ AutoUpdater.hpp
@@ -60,9 +60,7 @@ uintptr_t getAddress(URLSession session, const std::string& addrName, int fileTy
 
     for (const auto& result : results) 
     {
-        std::string resultStr;
-
-        resultStr = ReplaceArray(result, { "=", ";" }, "");
+        std::string resultStr = ReplaceArray(result, { "=", ";" }, "");
 
         return static_cast<uintptr_t>(std::stoull(resultStr, nullptr, 16));
     }
